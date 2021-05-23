845d6d485dca1fd4a36cf154982633684de71dfb
AllenKd <kaodondon7@gmail.com>
2021-05-23 18:23:04 +0800

    refine banker

--- internal/pkg/banker/banker.go
+++ internal/pkg/banker/banker.go
@@ -1,17 +1,23 @@
 package banker
 
 import (
-	"KaiJi-Casino/internal/pkg/cache"
 	"KaiJi-Casino/internal/pkg/db"
 	"KaiJi-Casino/internal/pkg/db/collection"
-	"KaiJi-Casino/internal/pkg/util"
 	"fmt"
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
-	"reflect"
 	"sync"
+	"time"
 )
+
+const (
+	JUDGE_RESULT_BANKER  JudgeResult = "banker"  // banker win, gambler lose
+	JUDGE_RESULT_GAMBLER JudgeResult = "gambler" // gambler win, banker lose
+	JUDGE_RESULT_TIE     JudgeResult = "tie"
+)
+
+type JudgeResult string
 
 type banker struct{}
 
@@ -28,104 +34,128 @@ func New() *banker {
 	return instance
 }
 
-func (b banker) GetGambleInfo(gameId primitive.ObjectID) (collection.SportsData, error) {
-	return db.New().GetGambleInfo(gameId)
-}
+func (b banker) GetGames(gameType collection.GameType, begin time.Time, end time.Time) (games []collection.SportsGameResult, err error) {
+	// TODO: consider result exposure
+	log.Debug("get games")
 
-func (b banker) GetGameResult(gameId primitive.ObjectID) (gambleInfo collection.GambleInfo, err error) {
-	log.Debug("get game result: ", gameId.Hex())
-
-	c, _ := cache.New().Get(gameId.Hex())
-	if c != nil {
-		log.Debug("had cached data")
-		obj, err := util.BytesToStruct(c)
-		if err != nil {
-			log.Error("fail to convert cached data to struct: ", err.Error())
-		} else {
-			gambleInfo = obj.(collection.GambleInfo)
-			return
+	var filter bson.M
+	if gameType != collection.GAME_TYPE_ALL {
+		filter = bson.M{
+			"type": gameType,
+			"start_time": bson.M{
+				"$gt": begin,
+				"$lt": end,
+			},
+		}
+	} else {
+		filter = bson.M{
+			"start_time": bson.M{
+				"$gt": begin,
+				"$lt": end,
+			},
 		}
 	}
 
+	return db.New().GetGames(filter, nil)
+}
+
+func (b banker) GetGambles(gameId *primitive.ObjectID) (gambles []collection.Gambling, err error) {
+	log.Debug("get gambles")
+
 	filter := bson.M{
-		"_id": gameId,
+		"game_id": gameId,
 	}
-	games, err := db.New().GetGames(filter, nil)
+
+	return db.New().GetGambles(filter, nil)
+}
+
+func (b banker) GetBettings(gamblingId *primitive.ObjectID) (bets []collection.Betting, err error) {
+	log.Debug("get bets")
+
+	filter := bson.M{
+		"gambling_id": gamblingId,
+	}
+
+	return db.New().GetBets(filter, nil)
+
+}
+
+func (b banker) Judge(gamblingId *primitive.ObjectID, bet collection.Bet, amount int) (result JudgeResult, reward float64, err error) {
+	log.Debug("judge")
+
+	gambling, err := db.New().GetGambling(gamblingId)
 	if err != nil {
-		log.Error("fail to get games: ", err.Error())
+		log.Error("fail to get gambling: ", err.Error())
 		return
 	}
 
-	if len(games) != 1 {
-		log.Error("unexpected match count: ", len(games))
-		err = fmt.Errorf("unexpected match count: %d", len(games))
-		return
-	}
-
-	gambleInfo = *games[0].GambleInfo
-
-	// make cache
-	data, err := util.StructToBytes(games[0].GambleInfo)
+	res, odds, err := b.gamblingResult(gambling)
 	if err != nil {
-		log.Error("fail to convert gamble info to byte array for cache: ", err.Error())
+		log.Error("fail to get gambling result: ", err.Error())
 		return
 	}
-	_ = cache.New().Set(gameId.Hex(), data)
+
+	if res == bet {
+		result = JUDGE_RESULT_GAMBLER
+		reward = odds * float64(amount)
+	} else {
+		result = JUDGE_RESULT_BANKER
+		reward = 0
+	}
 	return
 }
 
-func (b banker) Battle(decision collection.BetInfo) (isGamblerWin bool, err error) {
-	gameResult, err := b.GetGameResult(decision.GameId)
+func (b banker) gamblingResult(gambling collection.Gambling) (result collection.Bet, odds float64, err error) {
+	log.Debug("get gambling result: ", gambling.Id)
+
+	game, err := db.New().GetGame(gambling.GameId)
 	if err != nil {
-		log.Error("fail to get game result: ", err.Error())
+		log.Error("fail to get game: ", err.Error())
 		return
 	}
 
-	switch decision.GambleType {
-	case collection.GambleTypeOriginal:
-		isGamblerWin = *gameResult.Original.Judgement == decision.BetSide
-		return
-	case collection.GambleTypeTotalPoint:
-		isGamblerWin = *gameResult.TotalPoint.Judgement == decision.BetSide
-		return
-	case collection.GambleTypeSpreadPoint:
-		isGamblerWin = *gameResult.SpreadPoint.Judgement == decision.BetSide
-		return
+	// TODO: refine design
+	switch gambling.Type {
+	case collection.GAMBLING_TYPE_ORIGINAL:
+		if game.Guest.Score < game.Host.Score {
+			result = collection.BET_HOST
+		} else if game.Guest.Score > game.Host.Score {
+			result = collection.BET_GUEST
+		} else {
+			result = collection.BET_TIE
+		}
+
+	case collection.GAMBLING_TYPE_SPREAD_POINT:
+		gsp := gambling.GetProperty("guest_spread_point").(float64)
+		hsp := gambling.GetProperty("host_spread_point").(float64)
+
+		gsc := float64(game.Guest.Score) + gsp
+		hsc := float64(game.Host.Score) + hsp
+
+		if gsc < hsc {
+			result = collection.BET_HOST
+		} else if gsc > hsc {
+			result = collection.BET_GUEST
+		} else {
+			result = collection.BET_TIE
+		}
+
+	case collection.GAMBLING_TYPE_TOTAL_SCORE:
+		threshold := gambling.GetProperty("threshold").(float64)
+		totalScore := float64(game.Guest.Score + game.Host.Score)
+		if totalScore < threshold {
+			result = collection.BET_UNDER
+		} else if totalScore > threshold {
+			result = collection.BET_OVER
+		} else {
+			result = collection.BET_EQUAL
+		}
 	default:
-		log.Warn("no matched gamble type: ", decision.GambleType)
-		err = fmt.Errorf("no matched gamble type: %s", decision.GambleType)
-		return
-	}
-}
-
-func (b banker) GetResponse(bet collection.BetInfo) (response float64) {
-	log.Debug("get response: ", bet.String())
-
-	c, _ := cache.New().Get(bet.String())
-	if c != nil {
-		log.Debug("had cached data")
-		return util.BytesToFloat64(c)
-	}
-
-	data, err := b.GetGambleInfo(bet.GameId)
-	if err != nil {
-		log.Error("fail to get gamble info: ", err.Error())
-		return
-	}
-	r := reflect.ValueOf(data)
-	f := reflect.Indirect(r).FieldByName(bet.GambleType)
-	if reflect.ValueOf(f).IsZero() {
-		log.Debug("unbetable gamble: ", bet.String())
+		log.Warn("unhandled gambling type: ", gambling.Type)
+		err = fmt.Errorf("unhandled gambling type: %s", gambling.Type)
 		return
 	}
 
-	gameResp := reflect.Indirect(f).FieldByName("Response")
-	sideResp := reflect.Indirect(gameResp).FieldByName(bet.BetSide)
-	if reflect.ValueOf(sideResp).IsZero() {
-		log.Error("invalid response, ", bet.String())
-	}
-
-	response = reflect.Indirect(sideResp).Float()
-	_ = cache.New().Set(bet.String(), util.Float64ToBytes(response))
+	odds = gambling.GetOdds(result)
 	return
 }
