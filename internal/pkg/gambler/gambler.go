4e3e73b45dc5ac92f45686558d628960bc09fe0c
AllenKd <kaodondon7@gmail.com>
2021-06-20 22:14:59 +0800

    move getgamblers to loader

--- internal/pkg/gambler/gambler.go
+++ internal/pkg/gambler/gambler.go
@@ -2,13 +2,10 @@ package gambler
 
 import (
 	"KaiJi-Casino/internal/pkg/banker"
-	"KaiJi-Casino/internal/pkg/db"
 	"KaiJi-Casino/internal/pkg/db/collection"
-	"KaiJi-Casino/internal/pkg/strategy"
 	"KaiJi-Casino/internal/pkg/strategy/common"
 	"fmt"
 	log "github.com/sirupsen/logrus"
-	"go.mongodb.org/mongo-driver/bson/primitive"
 	"sync"
 	"time"
 )
@@ -67,37 +64,5 @@ func (g *Gambler) play(games []collection.SportsGameInfo) {
 func (g Gambler) OnBroken() {
 	log.Info("gambler: ", g.Id.Hex(), ". was broken.")
 	brokenChan <- true
-	return
-}
-
-func GetGamblers(simulationId *primitive.ObjectID) (gamblers []Gambler, err error) {
-	log.Debug("get gamblers by simulation id: ", simulationId.Hex())
-
-	gamblersData, dbErr := db.New().ListGambler(simulationId)
-	if dbErr != nil {
-		log.Error("fail to load gambler: ", dbErr.Error())
-		err = dbErr
-		return
-	}
-
-	for _, gamblerData := range gamblersData {
-		strategyData, dbErr := db.New().GetStrategyData(gamblerData.Id)
-		if dbErr != nil {
-			log.Error("fail to get strategyData: ", dbErr.Error())
-			err = dbErr
-			return
-		}
-		stg, sErr := strategy.GenStrategy(strategyData)
-		if sErr != nil {
-			log.Error("gail to init strategy: ", sErr.Error())
-			err = sErr
-			return
-		}
-
-		gamblers = append(gamblers, Gambler{
-			GamblerData: gamblerData,
-			Strategy:    stg,
-		})
-	}
 	return
 }
